efc8b00244d2b2a8476455ce2430993f3d396b73
Darren Baker <darren@baker.sh>
2021-07-31 20:56:20 +0200

    ORA error code not always visible
    When a user defined error message is used then the ORA error code is
    not visible.
    Added a new column to always show the ORA error code

--- error_handler.sql
+++ error_handler.sql
@@ -1,87 +1,93 @@
--- cleanup table if it exists
-BEGIN
-   EXECUTE IMMEDIATE 'DROP TABLE db_error_log';
-EXCEPTION
-   WHEN OTHERS THEN
-      IF SQLCODE <> -942 THEN
-         RAISE;
-      END IF;
-END;
+/*
+ cleanup table if it exists
+*/
+begin
+  execute immediate 'drop table db_error_log';
+exception
+  when others then if sqlcode <> -942 then
+    raise;
+  end if;
+end;
 /
 
--- create the db_error_log table
-CREATE TABLE db_error_log
-(
-  err_function    VARCHAR2 (30)
- ,err_message     VARCHAR2 (4000)
- ,call_stack      VARCHAR2 (4000)
- ,back_trace      VARCHAR2 (4000)
- ,schemaname      VARCHAR2 (30)
- ,date_created    DATE
- ,user_created    VARCHAR2 (100)
+/*
+ create the db_error_log table
+*/
+create table db_error_log (
+   err_function varchar2(30)
+  ,err_code varchar2(10)
+  ,err_message varchar2(4000)
+  ,call_stack varchar2(4000)
+  ,back_trace varchar2(4000)
+  ,schemaname varchar2(30)
+  ,date_created date
+  ,user_created varchar2(100)
 );
 /
 
--- create the package
-CREATE OR REPLACE PACKAGE error_handler
-IS
-  -- error_handler raises this exception if the user requests an abort.
-  e_abort_failure    EXCEPTION;
-
-  en_abort_failure   PLS_INTEGER := -20999;
-  PRAGMA EXCEPTION_INIT (e_abort_failure, -20999);
-
-  PROCEDURE log_db_error (i_err_function   IN db_error_log.err_function%TYPE
-                         ,i_err_message    IN db_error_log.err_message%TYPE := NULL
-                         ,i_abort          IN BOOLEAN := FALSE
+/*
+ create the package
+*/
+create or replace package error_handler is
+  /* error_handler raises this exception if the user requests an abort. */
+  e_abort_failure exception;
+  en_abort_failure pls_integer := -20999;
+  pragma exception_init ( e_abort_failure,-20999 );
+  procedure log_db_error (i_err_function in db_error_log.err_function%type
+                         ,i_err_message in db_error_log.err_message%type := null
+                         ,i_abort in boolean := false
                          );
-END error_handler;
+
+end error_handler;
 /
 
-CREATE OR REPLACE PACKAGE BODY error_handler
-IS
-  PROCEDURE log_db_error (i_err_function   IN db_error_log.err_function%TYPE
-                         ,i_err_message    IN db_error_log.err_message%TYPE := NULL
-                         ,i_abort          IN BOOLEAN := FALSE
-                         )
-  IS
-    PROCEDURE insert_row
-    IS
-      PRAGMA AUTONOMOUS_TRANSACTION;
-    BEGIN
-      INSERT INTO db_error_log (err_function
+create or replace package body error_handler is
+
+  procedure log_db_error (i_err_function in db_error_log.err_function%type
+                         ,i_err_message in db_error_log.err_message%type := null
+                         ,i_abort in boolean := false
+                         ) is
+
+    procedure insert_row is
+      pragma autonomous_transaction;
+    begin
+      insert into db_error_log (err_function
+                               ,err_code
                                ,err_message
                                ,call_stack
                                ,back_trace
                                ,schemaname
                                ,date_created
                                ,user_created
-                               )
-           VALUES (i_err_function
-                  ,Substr (Nvl (i_err_message, Dbms_Utility.format_error_stack), 1, 4000)
-                  ,Substr (Dbms_Utility.format_call_stack, 1, 4000)
-                  ,Substr (Dbms_Utility.format_error_backtrace, 1, 4000)
-                  ,Sys_Context ('USERENV', 'SESSION_USER')
-                  ,Sysdate
-                  ,Sys_Context ('USERENV', 'OS_USER') || '@' || Sys_Context ('USERENV', 'HOST')
-                  );
+                               ) 
+      values (i_err_function
+             ,sqlcode
+             ,substr(nvl(i_err_message,dbms_utility.format_error_stack),1,4000)
+             ,substr(dbms_utility.format_call_stack,1,4000)
+             ,substr(dbms_utility.format_error_backtrace,1,4000)
+             ,sys_context('USERENV','SESSION_USER')
+             ,sysdate
+             ,sys_context('USERENV','OS_USER') || '@' || sys_context('USERENV','HOST')
+             );
 
-      COMMIT;
-    EXCEPTION
-      WHEN OTHERS THEN
-        -- Must rollback on exit from autonomous transaction.
-        -- Display generic message to indicate problem.
-        ROLLBACK;
-        Dbms_Output.put_line ('Unable to write to error log!');
-        Dbms_Output.put_line ('Error:');
-        Dbms_Output.put_line (Dbms_Utility.format_error_stack);
-    END insert_row;
-  BEGIN
+      commit;
+    exception
+      when others then
+        /* 
+         Must rollback on exit from autonomous transaction.
+         Display generic message to indicate problem.
+        */
+        rollback;
+        dbms_output.put_line('Unable to write to error log!');
+        dbms_output.put_line('Error:');
+        dbms_output.put_line(dbms_utility.format_error_stack);
+    end insert_row;
+
+  begin
     insert_row;
+    if i_abort then raise_application_error(en_abort_failure,'Abort exception invoked by developer!');
+    end if;
+  end log_db_error;
 
-    IF i_abort THEN
-      raise_application_error (en_abort_failure, 'Abort exception invoked by developer!');
-    END IF;
-  END log_db_error;
-END error_handler;
+end error_handler;
 /
